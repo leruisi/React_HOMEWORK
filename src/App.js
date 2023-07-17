7274fa8edc8d2e0706e24598aa159efb6ff3a1e8
leruisi <yuristovskaya7@gmail.com>
2023-07-17 23:45:37 +0300

    HomeWork_4_3

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import UserPage from './page/UsersPage/UserPage'
+const App = () => {
+    return (
+        <div>
+         <UserPage/>
 
+
+        </div>
+    );
+};
+
+export {App};
